f92da788c7d79f632b73e811beaeb8dbab8f17a5
Eduardo Fabri <eduardohfabri@gmail.com>
2025-03-12 14:14:18 -0300

    Atualizando CorsConfig para permitir outra rota

--- src/main/java/com/chrono/infra/security/configuration/CorsConfig.java
+++ src/main/java/com/chrono/infra/security/configuration/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOrigins("https://chrono-steel-six.vercel.app")
                         .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
